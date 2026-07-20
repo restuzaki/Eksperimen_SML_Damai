2fa68fa7f7277449c5d8a0b0e7b8602a0b8a0ef1
Damai <damaiputrayudha1@gmail.com>
2026-07-20 15:21:27 +0700

    Use new experiment V2 to force cloud artifact storage configuration

--- Workflow-CI/MLProject/modelling.py
+++ Workflow-CI/MLProject/modelling.py
@@ -25,7 +25,7 @@ def main():
     y = df['Label']
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    mlflow.set_experiment("Phishing_Detection_CI_Pipeline")
+    mlflow.set_experiment("Phishing_Detection_CI_Pipeline_V2")
 
     with mlflow.start_run() as run:
         print("Melatih model dengan parameter terbaik...")
